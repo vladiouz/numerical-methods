0dbfb166e0ff450333e1ae61300e7c522f6b6650
Petru-Vlad Ionescu <92223782+vladiouz@users.noreply.github.com>
2023-07-01 00:23:07 +0300

    Create task-2.m

---
+++ linear-regression/task-2.m
@@ -0,0 +1,8 @@
+source("./parse_data_set_file.m")
+source("./prepare_for_regression.m")
+source("./parse_csv_file.m")
+source("./linear_regression_cost_function.m")
+source("./gradient_descent.m")
+source("./normal_equation.m")
+source("./lasso_regression_cost_function")
+source("./ridge_regression_cost_function")
