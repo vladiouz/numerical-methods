c28a8df88e03b6c6d9ae42bd6148019dba271080
Petru-Vlad Ionescu <92223782+vladiouz@users.noreply.github.com>
2023-07-01 00:19:01 +0300

    Create task-1.m

---
+++ markov/task-1.m
@@ -0,0 +1,7 @@
+source("./parse_labyrinth.m")
+source("./get_adjacency_matrix.m")
+source("./get_link_matrix.m")
+source("./get_Jacobi_parameters.m")
+source("./perform_iterative.m")
+source("./heuristic_greedy.m")
+source("./decode_path.m")
